b84e8680061de813ae2ba1df69fe799410ef4271
Stephen Hayes <schayes4@gmail.com>
2023-07-07 17:32:58 -0400

    Updates

--- update.rb
+++ update.rb
@@ -30,8 +30,6 @@ subs = 0
 active = 0
 new_cust = 0
 
-print projects
-
 projects.each do |project_id, api_key|
   resp = get_overview_metrics(api_key: api_key, project_id: project_id)
   metrics = resp["metrics"]
